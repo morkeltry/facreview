0c9488a87b121992cafb2f769d54c118be63d410
rachaelcodes <rorourke.rsp@gmail.com>
2017-08-17 11:30:33 +0100

    added example reviews to database, including testing out averaging sql function relates #22

--- database/db_build.sql
+++ database/db_build.sql
@@ -30,7 +30,7 @@ CREATE TABLE workshops (
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
   ws_id INTEGER REFERENCES workshops(id),
-  value INTEGER NOT NULL
+  review_value INTEGER NOT NULL
 );
 
 CREATE TABLE votes (
@@ -43,6 +43,32 @@ INSERT INTO users (name,email,pw,admin,avatar) VALUES
   ('Yahia','yahia@fac.com','test','false','avatar2.jpg' ) ;
 
 INSERT INTO workshops (title,date,gh_link) VALUES
-  ('Express','2017-08-14','www.hi.com'),
-  ('Express-handlebars','2017-08-15','www.bye.com');
+  ('Introduction To Express','2017-08-14','www.hi.com'),
+  ('Express-workshop','2017-08-14','www.hi.com'),
+  ('Handlebars','2017-08-14','www.hi.com'),
+  ('CSS Morning Challenge','2017-08-15','www.hi.com'),
+  ('TDD Express','2017-08-15','www.hi.com'),
+  ('Express-handlebars Morning Challenge','2017-08-16','www.bye.com');
+
+INSERT INTO reviews (ws_id, review_value) VALUES
+  (1, 4),
+  (1, 5),
+  (2, 4),
+  (3, 3),
+  (2, 4),
+  (3, 2);
+
+INSERT INTO votes (user_id, ws_id) VALUES
+  (1, 1),
+  (1, 2),
+  (1, 3),
+  (2, 1),
+  (2, 2),
+  (2, 3);
+
+UPDATE workshops
+SET avg_review = (SELECT AVG(review_value)
+                     FROM reviews
+                     WHERE reviews.ws_id = workshops.id);
+
 COMMIT;
