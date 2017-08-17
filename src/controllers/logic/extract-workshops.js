6fa0ffdafde291ffcbeb9ec073402fb3cbb2326a
astroash <astroaisha@gmail.com>
2017-08-17 19:20:01 +0100

    fixed extract-workshop function relates #11 :hankey:

--- src/controllers/logic/extract-workshops.js
+++ src/controllers/logic/extract-workshops.js
@@ -1,32 +1,27 @@
 
 const extractWorkshops = (arr) => {
-  let finalArray = [];
 
-  let mondayArray = [];
-  let tuesdayArray = [];
-  let wednesdayArray = [];
-
-  const dateArray = arr.reduce((acc,element)=>{
+  const dateArray = arr.reduce((acc,element)=> {
     if (acc.indexOf(element.date.toISOString()) === -1) {
-      acc.push(element.date.toISOString());  
+      acc.push(element.date.toISOString());
     }
       return acc;
   },[])
 
-  // finalArray = dateArray.filter(function(date){
-  //
-  //   if (finalArray.indexOf(date) === -1) {
-  //     return date;
-  //   }
-  // })
-
-console.log(dateArray);
-
 // console.log(dateArray);
-// console.log('final array', finalArray);
 
-}
+  const finalArray = arr.reduce((acc, element) => {
+    if(element.date.toISOString() === dateArray[0]) {
+      acc[0].push(element)
+    } else if(element.date.toISOString() === dateArray[1]) {
+      acc[1].push(element)
+    } else {
+      acc[2].push(element)
+    }
+    return acc
+}, [[],[],[]])
+  // console.log(finalArray);
+};
 
-// extractWorkshops(testData);
 
 module.exports = extractWorkshops ;
